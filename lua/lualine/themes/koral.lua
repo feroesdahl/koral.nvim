9086c5676e214a8f59aa42c24a1afe3fd4922962
Frederik Erslev Roesdahl <fe@roesdahl.com>
2025-10-27 22:09:46 +0100

    feat: update lualine styling

--- lua/lualine/themes/koral.lua
+++ lua/lualine/themes/koral.lua
@@ -3,13 +3,9 @@ local colors = require("koral.colors")
 return {
 	normal = {
 		a = { bg = colors.cyan, fg = colors.black },
-		b = { bg = colors.black, fg = colors.blue },
+		b = { bg = colors.black, fg = colors.orange },
 		c = { bg = colors.black, fg = colors.white },
+		x = { bg = colors.black, fg = colors.purple },
+		y = { bg = colors.black, fg = colors.blue },
 	},
-
-	insert = { a = { bg = colors.orange, fg = colors.black } },
-	visual = { a = { bg = colors.blue, fg = colors.black } },
-	replace = { a = { bg = colors.pink, fg = colors.black } },
-	command = { a = { bg = colors.purple, fg = colors.black } },
-	inactive = { a = { bg = colors.grey, fg = colors.black } },
 }
