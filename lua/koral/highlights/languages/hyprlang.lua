6805f7e7922999e89b7b116e915cf1230098dfe1
Frederik Erslev Roesdahl <fe@roesdahl.com>
2025-11-29 14:13:32 +0100

    feat: update hyprlang colors

---
+++ lua/koral/highlights/languages/hyprlang.lua
@@ -0,0 +1,7 @@
+local colors = require("koral.colors")
+
+return {
+  ["@constant.builtin.hyprlang"] = { fg = colors.purple },
+  ["@property.hyprlang"] = { fg = colors.blue },
+  ["@operator.hyprlang"] = { fg = colors.white },
+}
