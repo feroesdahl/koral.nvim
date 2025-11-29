0c4ece9dc8aa564d8085d654eab48415d860c37f
Frederik Erslev Roesdahl <fe@roesdahl.com>
2025-11-29 11:21:24 +0100

    feat: update markdown colors including markview support

---
+++ lua/koral/highlights/plugins/markview.lua
@@ -0,0 +1,35 @@
+local colors = require("koral.colors")
+
+return {
+  MarkviewHeading1 = { link = "@markup.heading.1" },
+  MarkviewHeading2 = { link = "@markup.heading.2" },
+  MarkviewHeading3 = { link = "@markup.heading.3" },
+  MarkviewHeading4 = { link = "@markup.heading.4" },
+  MarkviewHeading5 = { link = "@markup.heading.5" },
+  MarkviewHeading6 = { link = "@markup.heading.6" },
+
+  MarkviewHeading1Sign = { link = "MarkviewHeading1" },
+  MarkviewHeading2Sign = { link = "MarkviewHeading2" },
+  MarkviewHeading3Sign = { link = "MarkviewHeading3" },
+  MarkviewHeading4Sign = { link = "MarkviewHeading4" },
+  MarkviewHeading5Sign = { link = "MarkviewHeading5" },
+  MarkviewHeading6Sign = { link = "MarkviewHeading6" },
+
+  MarkviewCode = { bg = colors.black },
+  MarkviewInlineCode = { bg = colors.black },
+
+  MarkviewListItemPlus = { fg = colors.blue },
+  MarkviewListItemStar = { fg = colors.blue },
+  MarkviewListItemMinus = { fg = colors.blue },
+
+  MarkviewQuoteDefault = { fg = colors.green },
+  MarkviewBlockQuoteDefault = { fg = colors.green },
+
+  MarkviewPalette0Sign = { fg = colors.light_grey },
+  MarkviewPalette1Sign = { fg = colors.cyan },
+  MarkviewPalette2Sign = { fg = colors.blue },
+  MarkviewPalette3Sign = { fg = colors.purple },
+  MarkviewPalette4Sign = { fg = colors.blue },
+  MarkviewPalette5Sign = { fg = colors.purple },
+  MarkviewPalette6Sign = { fg = colors.blue },
+}
