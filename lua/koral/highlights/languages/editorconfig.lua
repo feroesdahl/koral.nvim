f5dcdd7406a7ce290a1affb030255ea89dd1942b
Frederik Erslev Roesdahl <fe@roesdahl.com>
2026-03-14 12:05:57 +0100

    feat: add support for editorconfig

---
+++ lua/koral/highlights/languages/editorconfig.lua
@@ -0,0 +1,8 @@
+local colors = require("koral.colors")
+
+return {
+  ["@constant.editorconfig"] = { fg = colors.purple },
+  ["@operator.editorconfig"] = { fg = colors.white },
+  ["@property.editorconfig"] = { fg = colors.cyan },
+  ["@punctuation.bracket.editorconfig"] = { fg = colors.pink },
+}
