53902d3d0ea175e6f8fcf2f54c20eb5bc5ee1622
Frederik Erslev Roesdahl <fe@roesdahl.com>
2026-07-01 19:35:11 +0200

    feat: update editorconfig colors

--- lua/koral/highlights/languages/editorconfig.lua
+++ lua/koral/highlights/languages/editorconfig.lua
@@ -1,8 +1,9 @@
 local colors = require("koral.colors")
 
 return {
+  ["@character.special.editorconfig"] = { fg = colors.white },
   ["@constant.editorconfig"] = { fg = colors.purple },
   ["@operator.editorconfig"] = { fg = colors.white },
-  ["@property.editorconfig"] = { fg = colors.cyan },
-  ["@punctuation.bracket.editorconfig"] = { fg = colors.pink },
+  ["@property.editorconfig"] = { fg = colors.blue },
+  ["@punctuation.bracket.editorconfig"] = { fg = colors.cyan },
 }
